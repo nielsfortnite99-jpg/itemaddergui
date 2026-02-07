f1b28a6f1bfe63a12804729bea9855c6c54b526a
nielsfortnite99-jpg <nielsfortnite99@gmail.com>
2026-02-07 01:37:55 +0100

    Update presets.js

--- presets.js
+++ presets.js
@@ -1,9 +1,11 @@
 const presets = {
   shop: {
-    10: { item: 'itemadder:diamond_sword' },
-    11: { item: 'itemadder:gold_ingot' }
+    10: { item:'itemadder:diamond_sword' },
+    11: { item:'itemadder:gold_ingot' },
+    12: { color:'#00ff00' }
   },
   menu: {
-    13: { item: 'itemadder:menu_icon' }
+    13: { item:'itemadder:menu_icon' },
+    14: { color:'#ff00ff' }
   }
 };
